e812c716fce21ca0040311ee3def74ef9656110a
alexsza <alexandra_szabo@hotmail.com>
2023-06-28 23:04:02 +0100

    Timestamp working

--- Project1.sol
+++ Project1.sol
@@ -5,50 +5,44 @@ import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
 contract Project1 is NFTokenMetadata, Ownable {
-    event MintEvent(address indexed _to, uint256 indexed _tokenId, string _uri, uint256 _timestamp);
+    event MintEvent(address indexed _to, uint256 indexed _tokenId, string _uri, uint256 _mintTimestamp);
+    event TransferEvent(address indexed _from, address indexed _to, uint256 indexed _tokenId, uint256 _transferTimestamp);
 
-    mapping(uint256 => address) private tokenRecipients;
-    mapping(address => uint256) private recipientTokens;
-    uint256 private tokenCount;
+    mapping(uint256 => address) private tokenOwners;
+    mapping(uint256 => uint256) private tokenTransferTimestamps;
 
     constructor() {
         nftName = "CertiTrack";
         nftSymbol = "CT";
-        tokenCount = 0;
     }
 
-    function mint(
-        address _to,
-        uint256 _tokenId,
-        string calldata _uri
-    ) external onlyOwner {
-        super._mint(_to, _tokenId);
-        super._setTokenUri(_tokenId, _uri);
-        tokenRecipients[_tokenId] = _to;
-        recipientTokens[_to] = _tokenId;
-        tokenCount++;
+function mint(
+    address _to,
+    uint256 _tokenId,
+    string calldata _uri,
+    uint256 _transferTimestamp
+) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+    tokenOwners[_tokenId] = _to;
+    tokenTransferTimestamps[_tokenId] = _transferTimestamp; // Store the transfer timestamp
 
-        // Add the minting timestamp to the token's metadata
-        emit MintEvent(_to, _tokenId, _uri, block.timestamp);
-    }
+    emit MintEvent(_to, _tokenId, _uri, block.timestamp);
+}
 
-    function burn(uint256 _tokenId) external onlyOwner {
-        super._burn(_tokenId);
-        address recipient = tokenRecipients[_tokenId];
-        delete tokenRecipients[_tokenId];
-        delete recipientTokens[recipient];
-        tokenCount--;
-    }
+function transfer(address _to, uint256 _tokenId) external {
+    address from = tokenOwners[_tokenId];
 
-    function totalSupply() external view returns (uint256) {
-        return tokenCount;
-    }
+    require(from != address(0), "Token does not exist");
+    require(from == msg.sender, "Transfer not authorized");
 
-    function getTokenRecipient(uint256 _tokenId) external view returns (address) {
-        return tokenRecipients[_tokenId];
-    }
+    tokenOwners[_tokenId] = _to;
+    tokenTransferTimestamps[_tokenId] = block.timestamp; // Update the transfer timestamp
 
-    function getTokenByRecipient(address _recipient) external view returns (uint256) {
-        return recipientTokens[_recipient];
+    emit TransferEvent(from, _to, _tokenId, block.timestamp);
+}
+
+    function getTokenTransferTimestamp(uint256 _tokenId) external view returns (uint256) {
+        return tokenTransferTimestamps[_tokenId];
     }
 }
