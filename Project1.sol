ce55461fc2163a60ce0fa561357d455a1848b2c0
alexsza <alexandra_szabo@hotmail.com>
2023-06-28 12:03:43 +0100

    timestamp and diploma image added

--- Project1.sol
+++ Project1.sol
@@ -5,9 +5,16 @@ import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
 contract Project1 is NFTokenMetadata, Ownable {
+    event MintEvent(address indexed _to, uint256 indexed _tokenId, string _uri, uint256 _timestamp);
+
+    mapping(uint256 => address) private tokenRecipients;
+    mapping(address => uint256) private recipientTokens;
+    uint256 private tokenCount;
+
     constructor() {
-        nftName = "DigitalDiploma";
-        nftSymbol = "BS";
+        nftName = "CertiTrack";
+        nftSymbol = "CT";
+        tokenCount = 0;
     }
 
     function mint(
@@ -17,6 +24,31 @@ contract Project1 is NFTokenMetadata, Ownable {
     ) external onlyOwner {
         super._mint(_to, _tokenId);
         super._setTokenUri(_tokenId, _uri);
+        tokenRecipients[_tokenId] = _to;
+        recipientTokens[_to] = _tokenId;
+        tokenCount++;
+
+        // Add the minting timestamp to the token's metadata
+        emit MintEvent(_to, _tokenId, _uri, block.timestamp);
     }
 
+    function burn(uint256 _tokenId) external onlyOwner {
+        super._burn(_tokenId);
+        address recipient = tokenRecipients[_tokenId];
+        delete tokenRecipients[_tokenId];
+        delete recipientTokens[recipient];
+        tokenCount--;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return tokenCount;
+    }
+
+    function getTokenRecipient(uint256 _tokenId) external view returns (address) {
+        return tokenRecipients[_tokenId];
+    }
+
+    function getTokenByRecipient(address _recipient) external view returns (uint256) {
+        return recipientTokens[_recipient];
+    }
 }
